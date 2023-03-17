945f88f8d0422734fe61dd9411a6cb6b0bdf66bb
Maddouchhamza <maddouch.hamza@gmail.com>
2023-03-16 18:09:46 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -Start point
+ *
+ * Return: 0 as success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
