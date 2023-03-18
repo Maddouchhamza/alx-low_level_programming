c4d3acefc5c6b4c0b3a10894e26b6df37ddded2b
Maddouchhamza <maddouch.hamza@gmail.com>
2023-03-17 18:53:00 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,13 +14,16 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	if (n==0)
-        {
-                printf("%d is zero\n", n);
-        }
-	if (n<0)
-        {
+	else
+	{
+		if(n==0)
+        	{
+                	printf("%d is zero\n", n);
+        	}
+		else
+        	{
                 printf("%d is negative\n", n);
-        }
+        	}
+	}
 	return (0);
 }
