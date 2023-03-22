b449f8603045686178e85e938f351ec33d0cca67
Maddouchhamza <maddouch.hamza@gmail.com>
2023-03-22 16:48:06 -0700

    Tasks 8 9 10 11

---
+++ 0x02-functions_nested_loops/10-add.C
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ *@i: integers to add
+ *@j: second integer to add
+ * Return: addition result
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+}
