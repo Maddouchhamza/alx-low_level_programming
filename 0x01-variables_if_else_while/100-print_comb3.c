099c3bd62a3f9be88f33bc1891d38fde9be0ed24
Maddouchhamza <maddouch.hamza@gmail.com>
2023-03-23 17:15:17 -0700

    Bug Fixed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,25 +4,22 @@
  *
  * Return 0 always
  */
-int main(void)
+int main (void)
 {
-	int i, j;
-		
-	for(i = '0'; i <= '9'; i++)
+	int i, j;	
+	for (i = '0'; i <= '9'; i++)
 	{
 		for (j = '0'; j <= '9'; j++)
 		{
-			if(i < j)
+			if (i < j)
 			{
 				putchar(i);
-				putchar(j);
-				
-				if(i != '8')
+				putchar(j);	
+				if (i != '8')
 				{
 					putchar(',');
 					putchar(' ');
 				}
-			
 			}
 		}	
 	}
