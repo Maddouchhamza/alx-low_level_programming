42f9aa5b7bcea67fed72891c15073a725ec63150
Maddouchhamza <maddouch.hamza@gmail.com>
2023-03-27 09:33:25 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * swap_int - swap the values of two integers
+ * @a: adresse of the integer a
+ * @b: adresse of the integer b
+ *
+ * Return: no return
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = a;
+	a = b;
+	b = c;
+}
