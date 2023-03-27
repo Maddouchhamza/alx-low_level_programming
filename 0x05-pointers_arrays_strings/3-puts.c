47df604a04c962bc5512d867ea2376aac12f4bd5
Maddouchhamza <maddouch.hamza@gmail.com>
2023-03-27 10:28:47 -0700

    Task 3 done!

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - prints a string followed by a new line
+ * @str: adresse of a string
+ *
+ * Return: no return
+ */
+void _puts(char *str)
+{
+	char c = *(str++);
+
+	while (c != '\0')
+	{
+		_putchar(c);
+		c = *(str++);
+	}
+	_putchar('\n');	
+}
