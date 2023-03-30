fe4ec01af3532a7d0b295557715644492ed5b9bc
Maddouchhamza <maddouch.hamza@gmail.com>
2023-03-30 09:41:43 -0700

    Task 1 donegit add .

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncat - function that concatenates two strings
+ * @dest: adress of a string
+ * @src: adress of a second string
+ * @n: integer
+ *
+ * Return: pointer to a string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *first = dest;
+	int j, i = 0;
+
+	while (*first != '\0')
+	{
+		first++;
+		i++;
+	}
+	
+	for (j = 0; j < n; j++)
+	{
+		dest[i] = src[j]; 
+	}
+	return(dest);
+}
