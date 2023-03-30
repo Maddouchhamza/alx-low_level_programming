60ca3e6ded9046aa1602a20f915eba9739155bb9
Maddouchhamza <maddouch.hamza@gmail.com>
2023-03-29 18:53:29 -0700

    Fix bug Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,16 +8,16 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	char *first = dest; 
+	char *first = dest;
 	char *second = src;
 	char *result = first;
 	char *debut = result;
-	
+
 	while (*first != '\0')
 	{
 		*result = *first;
 		result++;
-		first++;	
+		first++;
 	}
 	while (*second != '\0')
 	{
@@ -26,4 +26,4 @@ char *_strcat(char *dest, char *src)
 		second++;
 	}
 	return (debut);
-}	
+}
