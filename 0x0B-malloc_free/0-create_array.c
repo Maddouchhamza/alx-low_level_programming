c1adbb741e20b6f6c7e9a990120c6f2fc531f150
Maddouchhamza <maddouch.hamza@gmail.com>
2023-04-11 14:43:30 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,6 +18,10 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	L = malloc(sizeof(char) * size);
+	if (L == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		L[i] = c;
