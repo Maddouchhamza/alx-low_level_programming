8322132e49cfd3f3ae76adcd648578d16360324f
Maddouchhamza <maddouch.hamza@gmail.com>
2023-04-11 15:38:10 -0700

    Task 1 done

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - function copy a string
+ * @str: adress to a string
+ *
+ * Return: pointer to the copy
+ */
+char *_strdup(char *str)
+{
+	int j, i = 0;
+	char *C;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	C = malloc(sizeof(char) * i);
+	for (j = 0; j < i; j++)
+	{
+		C[j] = *str++;
+	}
+	return (C);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,5 @@
 char *create_array(unsigned int size, char c);
 
 int _putchar(char c);
+
+char *_strdup(char *str);
