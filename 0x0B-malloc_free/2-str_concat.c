dbd1a8a8ff28e6774d6115635053886f8d4ea9ff
Maddouchhamza <maddouch.hamza@gmail.com>
2023-04-11 17:49:31 -0700

    Task 2 done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,11 +12,11 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, k = 0, m = 0;
 	char *F;
 
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s1 != NULL)
 	{
 		i++;
 	}
-	while (s2[j] != '\0')
+	while (s2[j] != '\0' && s2 != NULL)
 	{
 		j++;
 	}
