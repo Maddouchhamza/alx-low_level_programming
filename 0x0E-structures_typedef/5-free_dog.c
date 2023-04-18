5eeeeb50e71832190b3194e7dac9fee2c918e391
Maddouchhamza <maddouch.hamza@gmail.com>
2023-04-18 10:51:41 -0700

    taks 5 done

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dog - function that frees dogs
+ * @d: pointer to a dog_t dog
+ *
+ * Return: no return
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
