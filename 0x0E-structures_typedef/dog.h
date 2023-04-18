22b04db22fafa3348f475ebd86f40822a1ced09c
Maddouchhamza <maddouch.hamza@gmail.com>
2023-04-18 09:36:59 -0700

    taks 3 done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,4 +19,5 @@ struct dog
 struct dog *_malloc(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 #endif
