b7d78b5dffc6de8e4377ac4418e58a459db6163f
Maddouchhamza <maddouch.hamza@gmail.com>
2023-04-19 16:06:49 -0700

    taks 0 done

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,1 +1,17 @@
+include <stddef.h>
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
+
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
+void array_iterator(int *array, size_t size, void (*action)(int));
+void print_elem(int elem);
+void print_elem_hex(int elem);
+int int_index(int *array, int size, int (*cmp)(int));
+int is_98(int elem);
+int is_strictly_positive(int elem);
+int abs_is_98(int elem);
+
+#endif /* FUNCTION_POINTERS */
