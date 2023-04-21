670a735a04ad69d9bb663acc0d4fcb4117bfdd9b
Maddouchhamza <maddouch.hamza@gmail.com>
2023-04-21 08:00:14 -0700

    taks 0 done

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,6 @@
-#ifndef
-#include <stddef.h>
-#endif
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
+#include <stddef.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
