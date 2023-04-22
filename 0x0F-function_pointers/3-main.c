9d2dc9d0fcc9636d85a7308a3799a2dc85eec39e
Maddouchhamza <maddouch.hamza@gmail.com>
2023-04-21 18:48:12 -0700

    Task done finally

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (get_op_func(argv[2]) == NULL)
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
