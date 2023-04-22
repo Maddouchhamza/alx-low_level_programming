ad11b5d0044b6465be09420060b08fd9acbeb8c4
Maddouchhamza <maddouch.hamza@gmail.com>
2023-04-21 18:43:50 -0700

    Task done finally

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,12 +12,12 @@ int main(int argc, char *argv[])
 {
 	if (argc != 4)
 	{
-		printf("Eoooooooroor\n");
+		printf("Error\n");
 		exit(98);
 	}
 	if (get_op_func(argv[2]) == NULL)
 	{
-		printf("Eroor\n");
+		printf("Error\n");
 		exit(99);
 	}
 	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
