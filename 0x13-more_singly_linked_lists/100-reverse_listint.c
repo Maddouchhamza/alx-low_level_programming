80cec93346013a8169b7d8541b4101955cc3babc
Maddouchhamza <maddouch.hamza@gmail.com>
2023-05-02 18:38:00 -0700

    Task 9 done!

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,17 +7,16 @@
 */
 listint_t *reverse_listint(listint_t **head)
 {
-listint_t *next = NULL;
-listint_t *prev = NULL;
+	listint_t *next = NULL;
+	listint_t *prev = NULL;
 
-while (*head != NULL)
-{
-next = (*head)->next;
-(*head)->next = prev;
-prev = *head;
-*head = next;
-}
-*head = prev;
-
-return (*head);
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
+	return (*head);
 }
